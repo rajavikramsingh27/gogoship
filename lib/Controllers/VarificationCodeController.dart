0cb543c7ed358b7ce06d9210f9a62c392b6f8bd4
GranzaX Team <rajavikramsingh27@gmail.com>
2022-04-06 21:25:34 +0530

    merged

--- lib/Controllers/VarificationCodeController.dart
+++ lib/Controllers/VarificationCodeController.dart
@@ -1,4 +1,6 @@
 
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
@@ -9,7 +11,27 @@ class VerificationCodeController extends GetxController {
   Rx<TextEditingController> txtOTP_Third = TextEditingController().obs;
   Rx<TextEditingController> txtOTP_Fourth = TextEditingController().obs;
 
-  reset() {
+  FocusNode focusFirst = FocusNode();
+  FocusNode focusSecond = FocusNode();
+  FocusNode focusThird = FocusNode();
+  FocusNode focusFourth = FocusNode();
 
+  RxDouble height = 0.0.obs;
+
+  reset() {
+    Future.delayed(Duration(milliseconds: 50), () {
+      BuildContext contextScreen = Get.context!;
+      contextScreen;
+      height.value = MediaQuery.of(contextScreen).size.height -
+          (MediaQuery.of(contextScreen).padding.top + kToolbarHeight);
+    });
   }
+
+  authenticate() {
+    print(txtOTP_First.value.text);
+    print(txtOTP_Second.value.text);
+    print(txtOTP_Third.value.text);
+    print(txtOTP_Fourth.value.text);
+  }
+
 }
