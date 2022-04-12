74eb2bdb0dce3e261e0e7c08ef26674d3eb81903
rajavikramsingh27 <rajavikramsingh27@gmail.com>
2022-04-12 09:37:30 +0530

    apple signin

--- lib/Styles/Components.dart
+++ lib/Styles/Components.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:get/utils.dart';
 import 'package:gogoship/Styles/ColorStyle.dart';
 import 'package:gogoship/Styles/TextStyles.dart';
 import 'package:gogoship/Components/TextFields.dart';
@@ -7,7 +7,7 @@ import 'package:gogoship/Styles/ImageStyle.dart';
 import 'package:gogoship/Styles/SizeStyle.dart';
 
 import 'package:google_sign_in/google_sign_in.dart';
-
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class ComponentsTF extends StatelessWidget {
   final TextEditingController? controller;
@@ -38,9 +38,7 @@ class ComponentsTF extends StatelessWidget {
         TextFields(
           keyboardType: TextInputType.emailAddress,
           hintText: hintText,
-
           obscureText: obscureText,
-
         ),
       ],
     );
@@ -74,7 +72,6 @@ class ComponentsTFPassword extends StatelessWidget {
         TextFieldsPassword(
           keyboardType: TextInputType.text,
           hintText: hintText,
-
         ),
       ],
     );
@@ -82,36 +79,34 @@ class ComponentsTFPassword extends StatelessWidget {
 }
 
 class ComponentsAuthSubmit {
-  static submitAuth(String text,Function()? onTap ) {
-
+  static submitAuth(String text, Function()? onTap) {
     return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        primary: ColorStyle.secondaryColor,
-        fixedSize: Size(double.infinity, 60),
-        elevation: 2,
-        padding: EdgeInsets.only(
-          left: 40,
-          right: 40,
-        ),
+        style: ElevatedButton.styleFrom(
+          primary: ColorStyle.secondaryColor,
+          fixedSize: Size(double.infinity, 60),
+          elevation: 2,
+          padding: EdgeInsets.only(
+            left: 40,
+            right: 40,
+          ),
           shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(SizeStyle.radius),
+            borderRadius: BorderRadius.circular(SizeStyle.radius),
           ),
-    ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          SizedBox(
-            width: 20,
-          ),
-          Text(text, style: TextStyles.textSize_16),
-          Icon(
-            Icons.send_outlined,
-            color: ColorStyle.primaryColor,
-          )
-        ],
-      ),
-      onPressed: onTap
-    );
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              width: 20,
+            ),
+            Text(text, style: TextStyles.textSize_16),
+            Icon(
+              Icons.send_outlined,
+              color: ColorStyle.primaryColor,
+            )
+          ],
+        ),
+        onPressed: onTap);
   }
 }
 
@@ -152,45 +147,50 @@ class ComponentsSocialSignIn extends StatelessWidget {
             googleLogin();
           },
         ),
-        SizedBox(width: 26,),
+        SizedBox(
+          width: 26,
+        ),
         InkWell(
           child: Container(
-            width: width,
-            height: height,
-            decoration: BoxDecoration(
-                color: ColorStyle.primaryColor,
-                borderRadius: BorderRadius.circular(30),
-                boxShadow: [
-                  BoxShadow(
-                      offset: Offset(0, 3),
-                      blurRadius: 45,
-                      color: Colors.black.withOpacity(0.2))
-                ]),
-            alignment: Alignment.center,
-            child: Icon(Icons.facebook,
-            color: ColorStyle.fromHex("039BE5"),
-            )
-          ),
+              width: width,
+              height: height,
+              decoration: BoxDecoration(
+                  color: ColorStyle.primaryColor,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 3),
+                        blurRadius: 45,
+                        color: Colors.black.withOpacity(0.2))
+                  ]),
+              alignment: Alignment.center,
+              child: Icon(
+                Icons.facebook,
+                color: ColorStyle.fromHex("039BE5"),
+              )),
           onTap: () {},
         ),
-        SizedBox(width: 26,),
+        SizedBox(
+          width: 26,
+        ),
         InkWell(
           child: Container(
-            width: width,
-            height: height,
-            decoration: BoxDecoration(
-                color: ColorStyle.primaryColor,
-                borderRadius: BorderRadius.circular(30),
-                boxShadow: [
-                  BoxShadow(
-                      offset: Offset(0, 3),
-                      blurRadius: 45,
-                      color: Colors.black.withOpacity(0.2))
-                ]),
-            alignment: Alignment.center,
-            child: Icon(Icons.apple)
-          ),
-          onTap: () {},
+              width: width,
+              height: height,
+              decoration: BoxDecoration(
+                  color: ColorStyle.primaryColor,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 3),
+                        blurRadius: 45,
+                        color: Colors.black.withOpacity(0.2))
+                  ]),
+              alignment: Alignment.center,
+              child: Icon(Icons.apple)),
+          onTap: () {
+            appleSignIn();
+          },
         ),
       ],
     );
@@ -205,36 +205,32 @@ class ComponentsAuthBottom {
       decoration: BoxDecoration(
           color: ColorStyle.primaryColor,
           image: DecorationImage(
-              image: AssetImage(ImageStyle.bottomAuth),
-              fit: BoxFit.fill)),
+              image: AssetImage(ImageStyle.bottomAuth), fit: BoxFit.fill)),
       alignment: Alignment.center,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
             decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(30)
-            ),
+                color: Colors.white, borderRadius: BorderRadius.circular(30)),
             child: icon,
           ),
-          SizedBox(height: 10,),
+          SizedBox(
+            height: 10,
+          ),
           Text.rich(
             TextSpan(
               children: [
-                TextSpan(text: txtNormal,
+                TextSpan(
+                    text: txtNormal,
                     style: TextStyles.textSize_16.apply(
-                        fontSizeDelta: 1,
+                      fontSizeDelta: 1,
                       // fontFamily: 'GEDinarOne',
-                    )
-                ),
+                    )),
                 if (txtBold.isNotEmpty)
                   TextSpan(
                       text: txtBold,
-                      style: TextStyles.textSize_16.apply(
-                          fontWeightDelta: 2
-                      )
-                  ),
+                      style: TextStyles.textSize_16.apply(fontWeightDelta: 2)),
               ],
             ),
           ),
@@ -243,7 +239,6 @@ class ComponentsAuthBottom {
     );
   }
 }
-
 
 googleLogin() async {
   GoogleSignIn googleSignIn = GoogleSignIn(
@@ -264,5 +259,21 @@ googleLogin() async {
     print("error error error error error error error error error");
     print(error);
   }
+}
 
+appleSignIn() async {
+  try {
+    final credential = await SignInWithApple.getAppleIDCredential(
+      scopes: [
+        AppleIDAuthorizationScopes.email,
+        AppleIDAuthorizationScopes.fullName,
+      ],
+    );
+
+    print(credential);
+
+  } catch (error) {
+    print("error error error error error error error ");
+    print(error);
+  }
 }
